fefbded82ba489472fa63bc824ca6ded2fdef61d
videolab666 <97349540+videolab666@users.noreply.github.com>
2026-09-12 13:13:23 +0300

    Keep blank-page recovery independent from React

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
-import {AppErrorBoundary} from './components/AppErrorBoundary';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppErrorBoundary>
-      <App />
-    </AppErrorBoundary>
+    <App />
   </StrictMode>,
 );
